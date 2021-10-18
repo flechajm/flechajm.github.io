7fb0686e34cb5f9dd439fc6fd816157913c79e2d
Juan Manuel Flecha <flechajm@gmail.com>
2021-10-18 09:44:54 -0300

    New design

--- docs/js/common.js
+++ docs/js/common.js
@@ -2,8 +2,14 @@ let $window;
 
 $(document).ready(function () {
   $window = $(window);
-
   showWelcome();
+});
+
+$(function () {
+  $(".menu a").on("click", function () {
+    let toggle = $("#toggle");
+    toggle.prop("checked", !toggle.prop("checked"));
+  });
 });
 
 function isDevice() {
@@ -11,11 +17,26 @@ function isDevice() {
 }
 
 function showWelcome() {
-  $("#section-1")
-    .fadeTo("slow", 0.1, function () {
-      $(this).css("background-image", "url(../docs/images/portrait.jpg)");
-    })
-    .fadeTo(700, 1, function () {
-      $("#welcome").fadeIn("slow");
-    });
+  $("#welcome").fadeOut("fast", function () {
+    $(this).css("background-image", "url(../docs/images/portrait.jpg)");
+    $(this)
+      .fadeIn()
+      .queue(function () {
+        $(".welcome-body")
+          .fadeIn({
+            duration: 'slow',
+            start: function () {
+              $(this).css({
+                display: "flex",
+              });
+            },
+          })
+          .delay(400)
+          .queue(function () {
+            $(".social")
+              .css({ opacity: 0, visibility: "visible" })
+              .animate({ opacity: 1 }, 'slow');
+          });
+      });
+  });
 }
